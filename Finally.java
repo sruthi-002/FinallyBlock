9a069458d9b3bc56f6eb6f5acf7e6de7dcbe3dfa
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-02 12:12:36 +0530

    Add files via upload

---
+++ Finally.java
@@ -0,0 +1,35 @@
+﻿package com.company;
+import java.util.Scanner;
+class trycatch
+{
+    public  static void trycatchex()
+    {
+        int n;
+        float avg;
+        int sum;
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        sum =in.nextInt();
+        try {
+            avg = sum/n;
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("PLease enter the valid number");
+        }
+        finally{
+            System.out.println("You have an another chance");
+            n = in.nextInt();
+            sum= in.nextInt();
+            avg = sum/n;
+            System.out.println("The average is"+" "+avg);
+        }
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+        trycatch n = new trycatch();
+        n.trycatchex();
+    }
+}
